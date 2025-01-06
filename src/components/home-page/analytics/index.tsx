a0c64f4ed7c069379f4a8fdb0754157481dde35b
Sherif Okunade <o.d.alsherif@gmail.com>
2025-01-06 07:35:12 +0100

    fix: resp

--- src/components/home-page/analytics/index.tsx
+++ src/components/home-page/analytics/index.tsx
@@ -1,43 +1,60 @@
 import Button from "@/components/ui/button/btn";
 
 function Index() {
-    return (
-        <div className="bg-[#FDC653] w-screen ">
-            <div className="container mx-64 px-48 py-24">
-                <div className="flex flex-col lg:flex-row justify-between  gap-12 lg:gap-24">
-                    <div className="flex flex-col space-y-8">
-                        <div className="text-center lg:text-left">
-                            <p className="text-[#723A20] text-4xl font-semibold">12k+</p>
-                            <p className="text-black font-medium text-lg">Total Users</p>
-                        </div>
-                        <div className="text-center lg:text-left">
-                            <p className="text-[#924B29] font-bold text-4xl">5k+</p>
-                            <p className="text-black font-medium text-lg">Employers</p>
-                        </div>
-                        <div className="text-center lg:text-left">
-                            <p className="text-[#723A20] font-bold text-4xl">200k+</p>
-                            <p className="text-black font-medium text-lg">Happy Users</p>
-                        </div>
-                    </div>  
-                    <div className="text-center mr-48  max-w-[652px] lg:text-center space-y-8">
-                        <h3 className="font-medium text-3xl lg:text-3xl text-black">
-                            Ready to Transform Your Recruitment or Nail Your Next Role?
-                        </h3>
-                        <p className=" lg:text-lg font-normal">
-                            We specialise in delivering top-tier healthcare staffing solutions across the UK. As a trusted partner, we provide a seamless connection between care providers and highly skilled healthcare professionals, ensuring your staffing needs are fulfilled with precision and care.
-                        </p>
-                        <Button
-                            text={"Contact Us"}
-                            bgColor="bg-black"
-                            size="px-4 py-2 font-medium"
-                            textColor="text-white"
-                        />
-                    </div>
-                </div>
+  return (
+    <div className="bg-[#FDC653] w-screen">
+      <div className="container mx-auto px-6 sm:px-12 lg:px-24 py-12 sm:py-16 lg:py-24 ">
+        <div className="flex flex-col lg:flex-row justify-around gap-8 lg:gap-24">
+          <div className=" mx-auto gap-6 flex items-center lg:flex-col lg:space-y-8 text-center lg:text-left">
+            <div>
+              <p className="text-[#723A20] text-3xl sm:text-4xl font-semibold">
+                12k+
+              </p>
+              <p className="text-black font-medium text-base sm:text-lg">
+                Total Users
+              </p>
             </div>
-        </div>
+            <div>
+              <p className="text-[#924B29] font-bold text-3xl sm:text-4xl">
+                5k+
+              </p>
+              <p className="text-black font-medium text-base sm:text-lg">
+                Employers
+              </p>
+            </div>
+            <div>
+              <p className="text-[#723A20] font-bold text-3xl sm:text-4xl">
+                200k+
+              </p>
+              <p className="text-black font-medium text-base sm:text-lg">
+                Happy Users
+              </p>
+            </div>
+          </div>
 
-    );
+          {/* Right Section */}
+          <div className="max-w-[652px] mx-auto text-center space-y-8">
+            <h3 className="font-medium text-2xl sm:text-3xl text-black">
+              Ready to Transform Your Recruitment or Nail Your Next Role?
+            </h3>
+            <p className=" text-primary-text sm:text-lg font-normal">
+              We specialise in delivering top-tier healthcare staffing solutions
+              across the UK. As a trusted partner, we provide a seamless
+              connection between care providers and highly skilled healthcare
+              professionals, ensuring your staffing needs are fulfilled with
+              precision and care.
+            </p>
+            <Button
+              text={"Contact Us"}
+              bgColor="bg-black"
+              size="px-4 py-2 font-medium"
+              textColor="text-white"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
--- src/components/home-page/hero/index.tsx
+++ src/components/home-page/hero/index.tsx
@@ -10,9 +10,9 @@ import Button from "../../ui/button/btn";
 
 function Index() {
   return (
-    <div className="bg-[#FFFCF5] w-screen pt-12">
-      <div className="grid grid-cols-2 px-8 container mx-auto ">
-        <div className="relative col-span-2 md:col-span-1 py-8 h-[calc(60vh-100px)]">
+    <div className="bg-[#FFFCF5] pt-6 md:pt-12">
+      <div className="grid grid-cols-2 md:px-8 container mx-auto ">
+        <div className="relative col-span-2 md:col-span-1 py-8 lg:h-[calc(60vh-100px)]">
           <Image
             className="absolute top-0 left-0"
             src={floatingOne}
@@ -24,34 +24,34 @@ function Index() {
             alt="floating rectangle"
           />
           <Image
-            className="absolute bottom-16 right-16"
+            className="absolute bottom-20 md:bottom-16 right-4  md:right-16"
             src={floatingTwo}
             alt="floating arrow"
           />
-          <div className="pl-32 pr-8  pt-8">
-            <p className="text-5xl font-semibold leading-[57px] text-primary-text">
+          <div className="px-4 md:pl-32 pr-8  pt-8">
+            <p className="text-3xl md:text-5xl font-semibold leading-[40px] md:leading-[57px] text-primary-text">
               Transforming Healthcare Recruitment Across the UK
             </p>
-            <p className="text-xl font-medium text-[#242424] mt-6 mb-8 max-w-[80%]">
+            <p className=" md:text-xl font-medium text-[#242424] mt-6 mb-8 max-w-[80%]">
               Expertly Managing Contingent Workforces with Precision and Care
             </p>
             <div className="flex items-center gap-4">
               <Button
                 text="Find Your Next Role"
                 bgColor="bg-black"
-                size="px-3 py-2 text-lg font-medium"
+                size="px-2 py-2 md:px-3  md:text-lg font-medium"
                 textColor="text-white"
               />
               <Button
                 text="Hire Top Talents"
                 bgColor="bg-[#FCB828]"
-                size="px-3 py-2 text-lg font-medium"
+                size="px-3 py-2 md:px-3 md:text-lg font-medium"
                 textColor="text-black"
               />
             </div>
           </div>
         </div>
-        <div className="relative col-span-2 md:col-span-1 py-8 px-16 h-[calc(65vh-100px)]">
+        <div className="relative col-span-2 md:col-span-1 py-12 md:py-8 px-8 lg:px-16  lg:h-[calc(65vh-100px)]">
           <Image
             src={covid}
             alt="covid 19 frontline healthcare essential workers"
@@ -60,12 +60,12 @@ function Index() {
           <Image
             src={ellipse}
             alt="ellipse"
-            className="absolute bottom-8 left-12"
+            className="absolute bottom-0 md:bottom-8 left-12"
           />
           <Image
             src={polygon}
             alt="polygon"
-            className="absolute top-0 right-0"
+            className="absolute top-0 right-4 md:right-0"
           />
         </div>
       </div>
