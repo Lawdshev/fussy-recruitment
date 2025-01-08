a4da1058ea588fcbcdd7ca40eac012dd7de6cf9f
niyiola <niyiolawhy@gmail.com>
2025-01-08 17:29:22 +0100

    update on recruitment process

--- src/components/services/recruitment-process/hero.tsx
+++ src/components/services/recruitment-process/hero.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import WhatWeOffer from '@/components/shared/what-we-offer';
 import Index from '@/components/recruitment-process';
 import WhyFussy from '@/components/why-fussy';
+import ClientStory from '@/components/shared/client-story/client-story';
+import SectionWithImage from '../../goals/goal';
+import objects from '@/assets/bodyframe/OBJECTS.png';
 
 
 const Hero = () => {
     return (
         <div>
-            <Index/>
-                <WhatWeOffer/>
-            <WhyFussy/>
-            </div> 
-        
+            <Index />
+            <WhatWeOffer />
+            <WhyFussy />
+            <ClientStory story='“While there are undoubtedly many dedicated individuals working behind the scenes at Fussy Recruitment, we’ve had the privilege of working directly with Candice, our representative. Candice is incredibly efficient and responsive, whether it’s for long-term staffing planning or addressing last-minute emergency cover. Her professionalism and dedication have made a significant impact on our operations, allowing us to maintain seamless care delivery”'
+                name='Healthcare Organisation' />
+            <SectionWithImage buttonBgColor='bg-[#0C0C0C]'
+                buttonSize='px-3 py-2'
+                buttonText='Contact Us Now'
+                buttonTextColor='text-white'
+                description=". Whether you' re looking to fill temporary, permanent, or contract positions, our RPO service ensures you have the right people in the right roles at the right time"
+                imageAlt='health professional'
+                imageSrc={objects}
+                title='Let Fussy Recruitment Handle Your Hiring Needs' />
+        </div>
+
     );
 };
 
