ef5f73de2d8e7c1075bc58e11f38648f46189385
niyiola <niyiolawhy@gmail.com>
2025-01-09 15:51:41 +0100

    blog features

---
+++ src/app/blog-2/page.tsx
@@ -0,0 +1,12 @@
+import HealthcareTrends from '@/components/blog2'
+import React from 'react'
+
+const page=()=> {
+  return (
+    <div>
+        <HealthcareTrends/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/blog-3/page.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import HealthcareCompliance from "@/components/blog-3"
+
+const page=()=>{
+    return(
+    <HealthcareCompliance/>
+)}
+export default page
