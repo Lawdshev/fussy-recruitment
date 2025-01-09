0459d2bf522838e3a48fcd1bf940fb1232e84437
ibrahim <ibrahimsaibu2010@gmail.com>
2025-01-09 07:03:23 +0100

    link page

--- src/app/services/temporary-staffing/page.tsx
+++ src/app/services/temporary-staffing/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import PageTitle from "@/components/shared/page-title";
 import Image from "next/image";
 import React from "react";
@@ -8,39 +9,66 @@ import CareSettings from "@/components/shared/caresettings/care-setting";
 import ClientStory from "@/components/shared/client-story/client-story";
 import Button from "@/components/ui/button/btn";
 import objects from "@/assets/bodyframe/OBJECTS.png";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
   return (
     <div>
       <div className="container mx-auto pt-12 px-6 sm:px-12 lg:px-24">
         <PageTitle title="Temporary Staffing" />
-        
+
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 py-12">
           <div className="order-2 lg:order-1 col-span-1 mt-10 lg:mt-0 flex items-center justify-center">
             <p className="text-primary-text leading-[30px]">
-              At Fussy Recruitment, we specialise in providing immediate, short-term staffing solutions tailored to healthcare providers&apos; diverse needs. Our temporary staffing services are designed to ensure you have access to qualified healthcare professionals whenever and wherever you need them. Whether managing a care home, housing with care, children&apos;s services, specialist care units, live-in care, NHS facilities, or home care, our extensive network of pre-screened and compliant candidates is ready to step in immediately.
+              At Fussy Recruitment, we specialise in providing immediate,
+              short-term staffing solutions tailored to healthcare
+              providers&apos; diverse needs. Our temporary staffing services are
+              designed to ensure you have access to qualified healthcare
+              professionals whenever and wherever you need them. Whether
+              managing a care home, housing with care, children&apos;s services,
+              specialist care units, live-in care, NHS facilities, or home care,
+              our extensive network of pre-screened and compliant candidates is
+              ready to step in immediately.
             </p>
           </div>
           <div className="order-1 lg:order-2 col-span-1 flex items-center justify-center">
-            <Image src={healthProfessional} alt="healthcare professionals" className="rounded-2xl" />
+            <Image
+              src={healthProfessional}
+              alt="healthcare professionals"
+              className="rounded-2xl"
+            />
           </div>
         </div>
-        
+
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 py-12">
           <div className="col-span-1 flex items-center justify-center">
             <Image src={maleNurses} alt="male nurses" className="rounded-2xl" />
           </div>
           <div className="col-span-1 mt-10 lg:mt-0 flex items-center justify-center">
             <p className="text-primary-text leading-[30px]">
-              With Fussy Recruitment, you can trust that every temporary staff member is fully vetted, trained, and compliant with industry regulations. This means you can focus on delivering excellent care while we handle the staffing logistics. Whether you need coverage for a single shift or ongoing support, our temporary staffing solutions are here to help you navigate the complexities of healthcare staffing with ease and confidence.
+              With Fussy Recruitment, you can trust that every temporary staff
+              member is fully vetted, trained, and compliant with industry
+              regulations. This means you can focus on delivering excellent care
+              while we handle the staffing logistics. Whether you need coverage
+              for a single shift or ongoing support, our temporary staffing
+              solutions are here to help you navigate the complexities of
+              healthcare staffing with ease and confidence.
             </p>
           </div>
         </div>
-        
+
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 py-12">
           <div className="order-2 lg:order-1 col-span-1 mt-10 lg:mt-0 flex items-center justify-center">
             <p className="text-primary-text leading-[30px]">
-              Healthcare settings often face unexpected staffing challenges, from sudden increases in patient demand to staff absences. Our flexible, temporary staffing solutions allow you to fill shifts quickly, maintain care continuity, and uphold the highest service standards without compromising quality. Our team works closely with you to understand your specific needs, ensuring that the professionals we provide are not only skilled but also a perfect fit for your care environment.
+              Healthcare settings often face unexpected staffing challenges,
+              from sudden increases in patient demand to staff absences. Our
+              flexible, temporary staffing solutions allow you to fill shifts
+              quickly, maintain care continuity, and uphold the highest service
+              standards without compromising quality. Our team works closely
+              with you to understand your specific needs, ensuring that the
+              professionals we provide are not only skilled but also a perfect
+              fit for your care environment.
             </p>
           </div>
           <div className="order-1 lg:order-2 col-span-1 flex items-center justify-center">
@@ -50,7 +78,10 @@ const Page = () => {
       </div>
 
       <CareSettings />
-      <ClientStory story={ `"Having worked in the care industry for over 16 years, I’ve partnered with numerous agencies to meet the staffing needs of our health and social care facility. Without a doubt, Fussy Recruitment stands out as the best agency I’ve ever collaborated with."`} name="Healthcare Worker" />
+      <ClientStory
+        story={`"Having worked in the care industry for over 16 years, I’ve partnered with numerous agencies to meet the staffing needs of our health and social care facility. Without a doubt, Fussy Recruitment stands out as the best agency I’ve ever collaborated with."`}
+        name="Healthcare Worker"
+      />
 
       <div className="bg-[#FDC653] px-6 sm:px-12 lg:px-20">
         <div className="container flex flex-col lg:flex-row items-center justify-between lg:justify-center mx-auto pt-8 sm:pt-12 lg:pt-18 lg:gap-12">
@@ -59,10 +90,12 @@ const Page = () => {
               Let us help you achieve your staffing goals
             </h1>
             <p className="font-normal leading-[30px] text-[#0C0C0C] mt-4">
-              Partner with us to find the right talent, streamline recruitment, and build a stronger team.
+              Partner with us to find the right talent, streamline recruitment,
+              and build a stronger team.
             </p>
             <div className="flex items-center justify-center lg:justify-start mt-6">
               <Button
+                onClick={() => router.push("/consultation")}
                 text="Request Staffing"
                 bgColor="bg-[#000000]"
                 size="px-6 py-2 font-normal mx-auto"
--- src/components/navbar/nav-bar.tsx
+++ src/components/navbar/nav-bar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
@@ -7,22 +7,24 @@ import NavBarLink from "./nav-link";
 import Button from "../ui/button/btn";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
   // Animation variants
   const menuVariants = {
     hidden: { opacity: 0, y: -20 },
     visible: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 }
+    exit: { opacity: 0, y: -20 },
   };
 
   return (
     <nav className="bg-white border-b-2  sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Image src={logo} alt="logo" className="w-24" />
-        
+
         {/* Hamburger Menu Icon */}
         <div className="md:hidden">
           <button
@@ -43,6 +45,7 @@ export default function NavBar() {
         {/* Buttons */}
         <div className="hidden md:flex gap-3">
           <Button
+            onClick={() => router.push("/consultation")}
             text="Hire"
             bgColor="bg-white"
             size="px-4 text-sm"
@@ -72,7 +75,11 @@ export default function NavBar() {
           >
             <div className="flex flex-col text-gray-700 space-y-4 px-4 py-2">
               {routes.map((route) => (
-                <NavBarLink key={route.name} route={route} closeMenu={() => setMenuOpen(false)} />
+                <NavBarLink
+                  key={route.name}
+                  route={route}
+                  closeMenu={() => setMenuOpen(false)}
+                />
               ))}
             </div>
             <div className="flex flex-col gap-3 px-4 py-2">
