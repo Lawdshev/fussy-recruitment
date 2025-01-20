73f289b2d63dbd1113ed7fd13fd2993d58ca47c7
Sherif Okunade <o.d.alsherif@gmail.com>
2025-01-20 12:50:38 +0100

    responsiveness for application

--- src/components/forms/legalLabel.tsx
+++ src/components/forms/legalLabel.tsx
@@ -7,7 +7,7 @@ export const LegalLabel = ({
 }) => {
   return (
     <div className="flex flex-col space-y-6">
-      <p className="font-medium text-base leading-5">{label}</p>
+      <p className="font-medium text-base text-primary-text leading-5">{label}</p>
       <div className="flex space-x-4">{children}</div>
     </div>
   );
